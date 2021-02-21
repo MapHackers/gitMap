e692fc944a52e89d3c99b622a30fae58f86872d4
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-02-21 22:45:25 +0900

    LoginPage css change

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -24,127 +24,130 @@ function LoginPage(props) {
     const initialEmail = localStorage.getItem("rememberMe") ? localStorage.getItem("rememberMe") : '';
 
     return (
-        <Formik 
-            initialValues={{
-                email: initialEmail,
-                password: '',
-            }}
-            validationSchema={Yup.object().shape({
-                email: Yup.string()
-                    .email('Email is invalid')
-                    .required('Email is required'),
-                password: Yup.string()
-                    .min(6, 'Password must be at least 6 characters')
-                    .required('Password is required')
-            })}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    let dataToSubmit = {
-                        email: values.email,
-                        password: values.password
-                    }
+        <div style={{ backgroundColor: 'rgba(0,0,0,0.1)', width: '500px', height: '300px', display: 'table', margin: 'auto', marginTop: '100px'}}>
+            <Formik
+                initialValues={{
+                    email: initialEmail,
+                    password: '',
+                }}
+                validationSchema={Yup.object().shape({
+                    email: Yup.string()
+                        .email('Email is invalid')
+                        .required('Email is required'),
+                    password: Yup.string()
+                        .min(6, 'Password must be at least 6 characters')
+                        .required('Password is required')
+                })}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        let dataToSubmit = {
+                            email: values.email,
+                            password: values.password
+                        }
 
-                    dispatch(loginUser(dataToSubmit))
-                        .then(response => {
-                            if (response.payload.loginSuccess) {
-                                window.localStorage.setItem('userId', response.payload.userId)
-                                if (rememberMe === true) {
-                                    window.localStorage.setItem('rememberMe', values.id)
+                        dispatch(loginUser(dataToSubmit))
+                            .then(response => {
+                                if (response.payload.loginSuccess) {
+                                    window.localStorage.setItem('userId', response.payload.userId)
+                                    if (rememberMe === true) {
+                                        window.localStorage.setItem('rememberMe', values.id)
+                                    } else {
+                                        localStorage.removeItem('rememberMe')
+                                    }
+                                    props.history.push('/')
                                 } else {
-                                    localStorage.removeItem('rememberMe')
+                                    setFormErrorMessage('Check out your Account or Password again')
                                 }
-                                props.history.push('/')
-                            } else {
+                            })
+                            .catch(err => {
                                 setFormErrorMessage('Check out your Account or Password again')
-                            }
-                        })
-                        .catch(err => {
-                            setFormErrorMessage('Check out your Account or Password again')
-                            setTimeout(() => {
-                                setFormErrorMessage('')
-                            }, 3000)
-                        })
-                    setSubmitting(false)
-                }, 500)
-            }}
-        >
-            {props => {
-                const {
-                    values,
-                    touched,
-                    errors,
-                    isSubmitting,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                } = props
-                return (
-                    <div className="app" style= {{ display: 'table', margin: 'auto', marginTop: '100px' }}>
-                    <Title level={2}>Login</Title>
+                                setTimeout(() => {
+                                    setFormErrorMessage('')
+                                }, 3000)
+                            })
+                        setSubmitting(false)
+                    }, 500)
+                }}
+            >
+                {props => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        isSubmitting,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                    } = props
+                    return (
+                        <div className="app" style={{ display: 'table', margin: 'auto', marginTop: '40px', paddingBottom: '40px' }}>
+                            <Title level={2}>Login</Title>
 
-                        <form onSubmit={handleSubmit} style={{ width: '350px' }}>
+                            <form onSubmit={handleSubmit} style={{ width: '350px' }}>
 
-                            <Form.Item required>
-                                <Input
-                                    id='email'
-                                    prefix={<UserOutlined style={{ color: 'rgba(0,0,0,0.25' }} />}
-                                    placeholder="Enter your email"
-                                    type='email'
-                                    value={values.email}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={
-                                        errors.email && touched.email ? 'text-input error' : 'text-input'
-                                    }
-                                />
-                                {errors.email && touched.eamil && (
-                                    <div className="input-feedback">{errors.email}</div>
+                                <Form.Item required>
+                                    <Input
+                                        id='email'
+                                        prefix={<UserOutlined style={{ color: 'rgba(0,0,0,0.25' }} />}
+                                        placeholder="Enter your email"
+                                        type='email'
+                                        value={values.email}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={
+                                            errors.email && touched.email ? 'text-input error' : 'text-input'
+                                        }
+                                    />
+                                    {errors.email && touched.eamil && (
+                                        <div className="input-feedback">{errors.email}</div>
+                                    )}
+                                </Form.Item>
+
+                                <Form.Item required>
+                                    <Input
+                                        id='password'
+                                        prefix={<LockOutlined style={{ color: 'rgba(0,0,0,0.25' }} />}
+                                        placeholder="Enter your password"
+                                        type='password'
+                                        value={values.password}
+                                        onChange={handleChange}
+                                        onBlur={handleBlur}
+                                        className={
+                                            errors.password && touched.password ? 'text-input error' : 'text-input'
+                                        }
+                                    />
+                                    {errors.password && touched.password && (
+                                        <div className="input-feedback">{errors.password}</div>
+                                    )}
+                                </Form.Item>
+
+                                {formErrorMessage && (
+                                    <label > <p style={{ color: '#ff000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadious: '10px' }}{...formErrorMessage}></p></label>
                                 )}
-                            </Form.Item>
 
-                            <Form.Item required>
-                                <Input
-                                    id='password'
-                                    prefix={<LockOutlined style={{ color: 'rgba(0,0,0,0.25' }} />}
-                                    placeholder="Enter your password"
-                                    type='password'
-                                    value={values.password}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    className={
-                                        errors.password && touched.password ? 'text-input error' : 'text-input'
-                                    }
-                                />
-                                {errors.password && touched.password && (
-                                    <div className="input-feedback">{errors.password}</div>
-                                )}
-                            </Form.Item>
-
-                            {formErrorMessage && (
-                                <label > <p style={{ color: '#ff000bf', fontSize: '0.7rem', border: '1px solid', padding: '1rem', borderRadious: '10px' }}{...formErrorMessage}></p></label>
-                            )}
-
-                            <Form.Item>
-                                <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe}> Remember me </Checkbox>
-                                <a className="login-form-forgot" href="/reset_user" style={{ float: "right" }}>
-                                    forgot password
+                                <Form.Item>
+                                    <Checkbox id="rememberMe" onChange={handleRememberMe} checked={rememberMe}> Remember me </Checkbox>
+                                    <a className="login-form-forgot" href="/reset_user" style={{ float: "right" }}>
+                                        forgot password
                                 </a>
-                                <div>
-                                    <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
-                                        Login
+                                    <div>
+                                        <Button type="primary" htmlType="submit" className="login-form-button" style={{ minWidth: '100%' }} disabled={isSubmitting} onSubmit={handleSubmit}>
+                                            Login
                                     </Button>
-                                </div>
+                                    </div>
                                 Or <a href="/register"> register now ! </a>
-                            </Form.Item>
+                                </Form.Item>
 
-                        </form>
+                            </form>
 
-                    </div>
+                        </div>
 
-                )
-            }
-            }
-        </Formik>
+                    )
+                }
+                }
+            </Formik>
+        </div>
+
     )
 }
 
