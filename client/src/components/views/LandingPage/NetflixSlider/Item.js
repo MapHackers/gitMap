8d20e1b147a9f7b342708357f034640055be1af8
Choi-Yong-Yeol <88dydfuf@naver.com>
2021-02-24 23:43:59 +0900

    slider item 컴포넌트의 위치 가져오기

--- client/src/components/views/LandingPage/NetflixSlider/Item.js
+++ client/src/components/views/LandingPage/NetflixSlider/Item.js
@@ -1,31 +1,43 @@
 import React from 'react';
 import SliderContext from './context'
 import './Item.scss'
+import ItemAtom from './ItemAtom'
 
-const Item = ({ card }) => (
-    <SliderContext.Consumer>
-        {({ elementRef }) => {
-            let isHovering = false;
-            return (
-                <div
-                    ref={elementRef}
-                    className={'item'}
-                    onMouseOver={() => {
-                        isHovering = null;
-                        console.log(isHovering)
-                    }}
-                    onMouseOut={() => {
-                        isHovering = false;
-                        console.log(isHovering)
-                    }}
-                >
-                    <a href={`/mapinfo/${card.id}`}>
-                        <img src={card.image} alt="" />
-                    </a>
-                </div>
-            );
-        }}
-    </SliderContext.Consumer>
-);
+function Item({ card, onItemHover }) {
+    const handleHover = (e) => {
+        const data = {
+            // offsetHeight : e.currentTarget.parentElement.children[card.id-1].offsetHeight,
+            // offsetWidth : e.currentTarget.parentElement.children[card.id-1].offsetWidth,
+            // sliderOffsetWidth : e.currentTarget.parentElement.offsetWidth,
+            // sliderOffsetHeight : e.currentTarget.parentElement.offsetHeight,
+            // sliderOffsetX : e.currentTarget.parentElement.parentElement.parentElement.offsetLeft,
+            // sliderOffsetY : e.currentTarget.parentElement.parentElement.parentElement.offsetTop,
+            rect: e.currentTarget.parentElement.children[card.id-1].getBoundingClientRect(),
+        }
+        onItemHover(data)
+    }
+    return (
+        <SliderContext.Consumer>
+            {({ elementRef }) => {
+                let isHovering = false;
+                return (
+                    <div
+                        ref={elementRef}
+                        className={'item'}
+                        onMouseOver={handleHover}
+                        onMouseOut={() => {
+                            isHovering = false;
+                            console.log(isHovering)
+                        }}
+                    >
+                        <a href={`/mapinfo/${card.id}`}>
+                            <img src={card.image} alt="" />
+                        </a>
+                    </div>
+                );
+            }}
+        </SliderContext.Consumer>
+    )
+}
 
-export default Item;
+export default Item
